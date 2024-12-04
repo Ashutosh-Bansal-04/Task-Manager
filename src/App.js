b2452791602078b53184ad811b36753e3561fe84
Ashutosh Bansal <ashutoshbansal0410@gmail.com>
2024-12-04 07:44:57 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { CssBaseline } from "@mui/material";
 import store from "./store";
 import TaskDashboard from "./pages/TaskDashboard";
+import TaskHome from "./pages/TaskHome";
 
 const App = () => (
   <Provider store={store}>
     <CssBaseline />
     <Router>
       <Routes>
+        <Route path="/" element={<TaskHome />} />
         <Route path="/tasks" element={<TaskDashboard />} />
       </Routes>
     </Router>
---
+++ src/pages/TaskHome.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TaskHome = () => {
+  return (
+    <div>
+      To view the Task Manager Dashboard <br /> Go to{" "}
+      <a href="http://localhost:3000/tasks">http://localhost:3000/tasks</a>
+    </div>
+  );
+};
+
+export default TaskHome;
